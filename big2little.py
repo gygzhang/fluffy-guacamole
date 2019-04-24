c554e492ebf7a88862b40d0f2797d4644f4c3c01
creaqi <577761192@qq.com>
2019-04-24 19:42:05 +0800

    convert for 12bit read

---
+++ big2little.py
@@ -0,0 +1,19 @@
+
+def big2little(src,dst):
+    stack = list()
+    # print(""==bytes(0))
+    byte = 0
+    fs = open(dst, "wb")
+    f = open(src, "rb")
+    # == and is has g great difference
+    while byte != b'':
+        byte = f.read(1);
+        stack.append(byte)
+        byte = f.read(1);
+        stack.append(byte);
+        bw = stack.pop()
+        fs.write(bw)
+        bw = stack.pop()
+        fs.write(bw)
+    fs.close()
+    f.close()
